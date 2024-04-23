bb44e98f68bb44a5d0cba96bbd469363b2214143
LoiNguyennn <nguyenphucloi231@gmail.com>
2024-04-23 20:54:23 +0700

    Done 1 & 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import fileinput
 from itertools import combinations
-from pysat.solvers import Solver
+from pysat.solvers import Glucose3
 
 DIRECTION = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
@@ -15,32 +15,26 @@ def SurroundingCells(board, x, y):
     for dir in DIRECTION:
         x1 = x + dir[0]
         y1 = y + dir[1]
-        if x1 >= 0 and x1 < len(board) and y1 >= 0 and y1 < len(board[0]):
+        if x1 >= 0 and x1 < len(board) and y1 >= 0 and y1 < len(board[0]) and board[x1][y1] == '_':
             surrounding_cells.append((x1, y1))
     return surrounding_cells
         
 def CellID(board, cell):
     return cell[0] * len(board[0]) + cell[1] + 1
 
-def GenerateDNFs(board):
-    clauses = []
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] != '_':
-                clauses.append([-CellID(board, (i, j))])
-
-                surrounding_cells = SurroundingCells(board, i, j)
-                for combination in combinations(surrounding_cells, int(board[i][j])):
-                    clause = []
-                    for cell in surrounding_cells:
-                        cell_id = CellID(board, cell)
-                        if cell in combination:
-                            clause.append(cell_id)  # cell is a trap
-                        else:
-                            clause.append(-cell_id)  # cell is not a trap
-                    clauses.append(clause)
-               
-    return clauses
+def GenerateDNF(board, i, j):
+    dnf = []
+    surrounding_cells = SurroundingCells(board, i, j)
+    for combination in combinations(surrounding_cells, int(board[i][j])):
+        clause = []
+        for cell in surrounding_cells:
+            cell_id = CellID(board, cell)
+            if cell in combination:
+                clause.append(cell_id)  # cell is a trap
+            else:
+                clause.append(-cell_id)  # cell is not a trap
+        dnf.append(clause)  
+    return dnf
 
 def GenerateTruthTable(dnf_clauses):
     num_variables = len(set([abs(literal) for clause in dnf_clauses for literal in clause]))
@@ -52,25 +46,53 @@ def GenerateTruthTable(dnf_clauses):
     
     return truth_table
 
-def CheckingTruthTable(truth_table, dnf_clauses):
+def GenerateCNF(truth_table, dnf_clauses):
     #return an array of True/False values
-    return [all([any([truth_table[i][abs(literal) - 1] if literal > 0 else not truth_table[i][abs(literal) - 1] for literal in clause]) for clause in dnf_clauses]) for i in range(len(truth_table))]
+    cnf = []
+    i = 0
+    for row in truth_table:
+        OR = False
+        for clause in dnf_clauses:
+            sorted_literal_index = sorted([abs(literal) for literal in clause])
+
+            AND = True
+            for literal in clause:
+                idx = sorted_literal_index.index(abs(literal))
+                AND &= row[idx] if literal > 0 else not row[idx]
+            OR |= AND
+        if OR == False:
+            cnf.append(de_morgan([sorted_literal_index[literal] if truth_table[i][literal] == 1 else -1 * sorted_literal_index[literal] for literal in range(len(truth_table[i]))]))
+        i += 1
+    return cnf
 
 def de_morgan(clause):
     return [-literal for literal in clause]
 
-def cnf_generator(truth_table, result):
-    cnf = []
-    for i in range(len(truth_table)):
-        if result[i] == False:
-            cnf.append(de_morgan([literal + 1 if truth_table[i][literal] == 1 else -(literal + 1) for literal in range(len(truth_table[i]))]))
-    return cnf
+def GetAnswer(board):
+    cnfs = []
+    existed = set() # remove duplicate
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] != '_':
+                dnf = GenerateDNF(board, i, j)
+                truth_table = GenerateTruthTable(dnf)
+                cnf = GenerateCNF(truth_table, dnf)
+                for clause in cnf:
+                    if tuple(clause) in existed:
+                        cnf.pop(cnf.index(clause))
+                    else:
+                        existed.add(tuple(clause))
+                cnfs.append(cnf)
+                
+    solver = Glucose3()
+    for cnf in cnfs:
+        for clause in cnf:
+            solver.add_clause(clause)
+    if solver.solve():
+        return solver.get_model()
+    return None
 
 if __name__ == '__main__':
     board = InputBoard()
-    dnfs = GenerateDNFs(board)
-    cnfs = []
-    truth_table = GenerateTruthTable(dnfs)
-    result = CheckingTruthTable(truth_table, dnfs)
-    cnfs = cnf_generator(truth_table, result)
-    print(cnfs)
+    answer = GetAnswer(board)
+    print(answer)
--- test.py
+++ test.py
@@ -5,12 +5,20 @@ def GenerateTruthTable(dnf_clauses):
         truth_table.append([])
         for j in range(num_variables):
             truth_table[-1].append((i >> j) & 1)
-    
     return truth_table
 
 def CheckingTruthTable(truth_table, dnf_clauses):
     #return an array of True/False values
-    return [all([any([truth_table[i][abs(literal) - 1] if literal > 0 else not truth_table[i][abs(literal) - 1] for literal in clause]) for clause in dnf_clauses]) for i in range(len(truth_table))]
+    res = []
+    for row in truth_table:
+        OR = False
+        for clause in dnf_clauses:
+            AND = True
+            for literal in clause:
+                AND &= row[abs(literal) - 1] if literal > 0 else not row[abs(literal) - 1]
+            OR |= AND
+        res.append(OR)
+    return res
 
 def de_morgan(clause):
     return [-literal for literal in clause]
@@ -22,7 +30,7 @@ def cnf_generator(truth_table, result):
             cnf.append(de_morgan([literal + 1 if truth_table[i][literal] == 1 else -(literal + 1) for literal in range(len(truth_table[i]))]))
     return cnf
 
-dnf_clauses = [[1, 2], [-3, 4], [5, -6], [-7, -8]]
+dnf_clauses = [[1, 2]]
 
 truth_table = GenerateTruthTable(dnf_clauses)
 truth_table_result = CheckingTruthTable(truth_table, dnf_clauses)
@@ -30,3 +38,6 @@ truth_table_result = CheckingTruthTable(truth_table, dnf_clauses)
 cnf_clauses = cnf_generator(truth_table, truth_table_result)
 
 print(cnf_clauses)
+for row in truth_table:
+    print(row)
+print(truth_table_result)
