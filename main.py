e7d89b8c4de469ac32ed37448fe4dac129c2e50c
LoiNguyennn <nguyenphucloi231@gmail.com>
2024-04-28 10:45:09 +0700

    bruteforce algorithm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import fileinput
-from itertools import combinations
+from itertools import combinations, product
 from pysat.solvers import Glucose3
 import random
 import pygame
@@ -137,6 +137,54 @@ def choose_var(clause, assignment):
             var = abs(x) - 1
     return var
 
+def PySat(board):
+    cnfs = []
+    existed = set() # remove duplicate
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] != '_':
+                dnf = GenerateDNF(board, i, j)
+                truth_table = GenerateTruthTable(dnf)
+                cnf = GenerateCNF(truth_table, dnf)
+                cnf.append([-CellID(board, (i, j))])
+                for clause in cnf:
+                    if tuple(clause) in existed:
+                        cnf.pop(cnf.index(clause))
+                    else:
+                        existed.add(tuple(clause))
+                cnfs.append(cnf)
+    solver = Glucose3()
+    for cnf in cnfs:
+        for clause in cnf:
+            solver.add_clause(clause)
+    
+    if solver.solve():
+        assignment = solver.get_model()
+        assignment = convertResult(assignment, board)
+        return assignment
+    return None
+    
+def brute_force_sat(cnf):
+    # Get all the variables in the CNF
+    variables = {abs(x) for clause in cnf for x in clause}
+
+    # Generate all possible assignments
+    for assignment in product([False, True], repeat=len(variables)):
+        assignment = {var: val for var, val in zip(variables, assignment)}
+
+        # Check if this assignment satisfies the CNF
+        if all(any(assignment[abs(x)] == (x > 0) for x in clause) for clause in cnf):
+            # If it does, return it
+            answer = []
+            for key in assignment:
+                if assignment[key] == True:
+                    answer.append(key)
+                else:
+                    answer.append(-key)
+            return answer
+    # If no satisfying assignment was found, return None
+    return None
+
 def bruteForce(board):
     cnfs = []
     existed = set() # remove duplicate
@@ -146,6 +194,7 @@ def bruteForce(board):
                 dnf = GenerateDNF(board, i, j)
                 truth_table = GenerateTruthTable(dnf)
                 cnf = GenerateCNF(truth_table, dnf)
+                cnf.append([-CellID(board, (i, j))])
                 for clause in cnf:
                     if tuple(clause) in existed:
                         cnf.pop(cnf.index(clause))
@@ -159,15 +208,10 @@ def bruteForce(board):
         for clause in cnf:
             clauses.append(clause)
             num_variables = max(num_variables, max(abs(l) for l in clause))
-    # solver = Glucose3()
-    # for cnf in cnfs:
-    #     for clause in cnf:
-    #         solver.add_clause(clause)
-    
-    # if solver.solve():
-    #     return solver.get_model()
-    assignment = walksat(board, clauses)
 
+    assignment = brute_force_sat(clauses)
+
+    # assignment = walksat(board, clauses)
     if assignment is None:
         return None
     assignment = convertResult(assignment, board)
@@ -267,9 +311,9 @@ def displayResult(_map):
 
 if __name__ == '__main__':
     board = InputBoard()
-    #print(board)
-    answer = getAnswer(board, backtracking)
-
+    # print(board)
+    answer = getAnswer(board, bruteForce)
+    print(answer)
     fo = open('output.txt', 'w')
     for i in range(len(board)):
         for j in range(len(board[0]) - 1):
